6bc93dc8f417a89aa1c90365a3978b7f189fd4df
kamioka24 <keee2404@gmail.com>
2021-07-28 23:56:12 +0900

    [Add]

--- app/src/main/java/com/example/freetest_app/MainActivity.kt
+++ app/src/main/java/com/example/freetest_app/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.freetest_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_url_test.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startButton.setOnClickListener {
+            val intent = Intent(this, UrlTestActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
