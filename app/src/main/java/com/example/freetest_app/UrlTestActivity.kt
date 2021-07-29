354339b57dee268f1de71f730821c0d1fc24a75e
kamioka24 <keee2404@gmail.com>
2021-07-30 08:11:49 +0900

    [Add]

--- app/src/main/java/com/example/freetest_app/UrlTestActivity.kt
+++ app/src/main/java/com/example/freetest_app/UrlTestActivity.kt
@@ -1,11 +1,10 @@
 package com.example.freetest_app
 
 import android.os.Bundle
-import android.text.util.Linkify
-import android.text.util.Linkify.TransformFilter
+import android.text.method.LinkMovementMethod
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import java.util.regex.Pattern
+import androidx.core.text.HtmlCompat
 
 class UrlTestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,14 +12,7 @@ class UrlTestActivity : AppCompatActivity() {
         setContentView(R.layout.activity_url_test)
 
         val urlTestTextView = findViewById<TextView>(R.id.urlTestTextView)
-        urlTestTextView.setText("あかさたなはまやらわ")
-        val pattern = Pattern.compile("なは")
-
-        addLink("https://www.youtube.com/", pattern, urlTestTextView)
-    }
-
-    private fun addLink(link: String, pattern: Pattern, textview: TextView) {
-        val filter = TransformFilter { match, url -> link }
-        Linkify.addLinks(textview, pattern, link, null, filter)
+        urlTestTextView.text = HtmlCompat.fromHtml(getString(R.string.url_test_text_view), HtmlCompat.FROM_HTML_MODE_COMPACT)
+        urlTestTextView.movementMethod = LinkMovementMethod.getInstance()
     }
 }
