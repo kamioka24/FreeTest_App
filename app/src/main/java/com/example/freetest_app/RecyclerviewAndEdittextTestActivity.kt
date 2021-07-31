f7c1b58d1992b3c5ddb5ad8bc912e449ec275ce1
kamioka24 <keee2404@gmail.com>
2021-07-31 11:38:10 +0900

    [Add]

---
+++ app/src/main/java/com/example/freetest_app/RecyclerviewAndEdittextTestActivity.kt
@@ -0,0 +1,11 @@
+package com.example.freetest_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecyclerviewAndEdittextTestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recyclerview_and_edittext_test)
+    }
+}
