8cf6ad10521ea584edb4511cf42938c4f7e5ec37
kamioka24 <keee2404@gmail.com>
2021-07-31 10:50:40 +0900

    [Add]

--- app/src/main/java/com/example/freetest_app/RecyclerviewTestActivity.kt
+++ app/src/main/java/com/example/freetest_app/RecyclerviewTestActivity.kt
@@ -1,33 +1,65 @@
 package com.example.freetest_app
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import java.util.zip.Inflater
 
 class RecyclerviewTestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recyclerview_test)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        val adapter = MyAdapter()
+
+        // どういう並び方をさせるかの設定
+        recyclerView.layoutManager = LinearLayoutManager(this) // 上から下に並べる
+        recyclerView.adapter = adapter
+
+        val textList = mutableListOf<String>()
+        for (i in 0..100) {
+            textList.add("要素$i")
+        }
+
+        // 上の textList を下の textList に入れる
+        adapter.textList = textList
+
+        // adapter の中身を変えたときに必ずこれを呼ぶ(必須)
+        // 呼ばれないと中身の変更が反映されない
+        adapter.notifyDataSetChanged()
     }
 
     private class MyAdapter : RecyclerView.Adapter<MyViewHolder>() {
+
+        var textList: List<String> = emptyList()
+
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-            TODO("Not yet implemented")
+            val inflater = LayoutInflater.from(parent.context)
+            val view = inflater.inflate(R.layout.list_item, parent, false)
+            return MyViewHolder(view)
         }
 
+        // View にデータを設定するためのメソッド(必須)
         override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-            TODO("Not yet implemented")
+            holder.textView.text = textList[position]
+            holder.itemView.setBackgroundColor(
+                if (position % 2 == 0) Color.WHITE else Color.LTGRAY
+            )
         }
 
-        override fun getItemCount(): Int {
-            TODO("Not yet implemented")
-        }
+        // アイテム総数メソッド(必須)
+        override fun getItemCount(): Int = textList.size
     }
 
     private class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        
+        val textView: TextView = itemView.findViewById(R.id.text_view)
     }
 }
