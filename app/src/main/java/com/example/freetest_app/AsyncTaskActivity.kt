5e87bc9279ed8d1a0e98f0228c8f996a28ef30f0
kamioka24 <keee2404@gmail.com>
2021-08-01 15:51:58 +0900

    [Add]

--- app/src/main/java/com/example/freetest_app/AsyncTaskActivity.kt
+++ app/src/main/java/com/example/freetest_app/AsyncTaskActivity.kt
@@ -1,11 +1,47 @@
 package com.example.freetest_app
 
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class AsyncTaskActivity : AppCompatActivity() {
+
+    private var task: MyAsyncTask? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_async_task)
+
+        task = MyAsyncTask().apply {
+            execute(100)
+        }
+    }
+
+    override fun onDestroy() {
+        task?.cancel(true)
+        task = null
+        super.onDestroy()
+    }
+
+    private inner class MyAsyncTask : AsyncTask<Int, String, String>() {
+
+        val textView = findViewById<TextView>(R.id.text_view)
+
+        override fun doInBackground(vararg params: Int?): String {
+            for (i in 1..params[0]!! ) {
+                publishProgress("loading...$i%")
+                Thread.sleep(50)
+            }
+            return "完了"
+        }
+
+        override fun onProgressUpdate(vararg values: String?) {
+            textView.text = values[0]
+        }
+
+        override fun onPostExecute(result: String?) {
+            textView.text = result
+        }
     }
 }
